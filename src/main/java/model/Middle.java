4ce6813f2e9de6c6fadae1f41e8f0d9b26bfc76e
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2023-05-07 17:29:16 +0200

    P42023T03-31 Implement Middle

--- src/main/java/model/Middle.java
+++ src/main/java/model/Middle.java
@@ -2,17 +2,20 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Middle {
     public List<Tile> tiles;
 
     List<Tile> getTiles(Color type){
-        return new ArrayList<>();
+        return tiles.stream()
+                .filter(tile -> tile == type)
+                .collect(Collectors.toList());
     }
-    void addTiles(List<Tile> tiles){
-
+    void addTiles(List<Tile> t){
+        tiles.addAll(t);
     }
     boolean hasTiles(Color type){
-        return false;
+        return tiles.contains(type);
     }
 }
