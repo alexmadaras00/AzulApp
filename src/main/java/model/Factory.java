218b1041e5743fe3b694dff71526fb0111b10d9d
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2023-05-08 14:22:02 +0200

    P42023T03-31 Fixed Bag Implemetation and Testing

--- src/main/java/model/Factory.java
+++ src/main/java/model/Factory.java
@@ -6,16 +6,17 @@ import java.util.stream.Collectors;
 
 public class Factory {
     public List<Tile> tiles;
-    List<Tile> getTiles(Color type){
-        return tiles.stream()
-                .filter(tile -> tile == type)
-                .collect(Collectors.toList());
+
+    List<Tile> getTiles(Color type) {
+        return new ArrayList<>();
     }
-    void addTiles(List<Tile> t){
-      tiles.addAll(t);
+
+    void addTiles(List<Tile> t) {
+
     }
-    boolean hasTiles(Color type){
-        return tiles.contains(type);
+
+    boolean hasTiles(Color type) {
+        return false;
     }
 
 }
--- src/test/java/BagTest.java
+++ src/test/java/BagTest.java
@@ -1,51 +1,57 @@
 import model.Bag;
 import model.Color;
 import model.Tile;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class BagTest {
-    Bag bag = new Bag();
-    List<Tile> tiles = new ArrayList<>();
-    @Before
-    public void setUp() {
+    static Bag bag;
+    static List<Tile> tiles = new ArrayList<>();
+
+    @BeforeAll
+    public static void setUp() {
+        bag = new Bag();
         tiles.add(Color.BLUE);
         tiles.add(Color.BLUE);
         tiles.add(Color.YELLOW);
         tiles.add(Color.RED);
+        assertTrue(bag.getTiles().size() <= 100);
     }
+
     @Test
     public void testGetTiles() {
         bag.addTiles(tiles);
         assertEquals(tiles, bag.getTiles());
+        assertTrue(bag.getTiles().size() <= 100);
+    }
+
+    @Test
+    public void testAddTiles() {
+        List<Tile> newTiles = new ArrayList<>();
+        newTiles.add(Color.CYAN);
+        newTiles.add(Color.RED);
+        bag.addTiles(newTiles);
+        assertEquals(6, bag.getTiles().size());
+        assertEquals(Color.CYAN, bag.getTiles().get(4));
+        assertEquals(Color.RED, bag.getTiles().get(5));
     }
     @Test
     public void testPopTiles() {
         int count = 2;
         assertTrue(tiles.size() >= count);
-        bag.addTiles(tiles);
         System.out.println(bag.getTiles());
         List<Tile> poppedTiles = bag.popTiles(count);
-        tiles.remove(tiles.size()-1);
-        tiles.remove(tiles.size()-1);
-        assertEquals(Color.BLUE, tiles.get(tiles.size() - 1));
+        System.out.println(bag.getTiles());
+        assertEquals(4, bag.getTiles().size());
+        assertEquals(Color.RED, tiles.get(tiles.size() - 1));
         assertEquals(tiles, poppedTiles);
-    }
-    @Test
-    public void testAddTiles(){
-        List<Tile> newTiles = new ArrayList<>();
-        newTiles.add(Color.CYAN);
-        newTiles.add(Color.RED);
-        bag.addTiles(newTiles);
-        assertEquals(2, bag.getTiles().size());
-        assertEquals(Color.CYAN, bag.getTiles().get(0));
-        assertEquals(Color.RED, bag.getTiles().get(1));
     }
 
 
