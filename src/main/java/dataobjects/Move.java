90c4c795a94dd6125a29c55920ad00f48bfe5412
TinyLittleWolf13 <j.c.degroot-3@student.utwente.nl>
2023-05-12 21:16:20 +0200

    P42023T03-78 Implemented and tested DataObjects classes (except Tile and its subclasses).
    In the classes I implemented getters, setters for all fields. Also I implemented constructors for no arguments and all arguments, and overwrote equals to be able to compare the objects. To test these I got the library BeanMatcher (added it to the dependecies)

--- src/main/java/dataobjects/Move.java
+++ src/main/java/dataobjects/Move.java
@@ -9,4 +9,67 @@ public class Move implements DataObject {
     private OptionalInt number;
     private Color type;
     private PlayerData player;
+
+    public Move() {
+    }
+    public Move(Boolean isFactory, OptionalInt number, Color type, PlayerData player) {
+        this.isFactory = isFactory;
+        this.number = number;
+        this.type = type;
+        this.player = player;
+    }
+    public Boolean getIsFactory() {
+        return isFactory;
+    }
+    public void setIsFactory(Boolean isFactory) {
+        this.isFactory = isFactory;
+    }
+    public OptionalInt getNumber() {
+        return number;
+    }
+    public void setNumber(OptionalInt number) {
+        this.number = number;
+    }
+    public Color getType() {
+        return type;
+    }
+    public void setType(Color type) {
+        this.type = type;
+    }
+    public PlayerData getPlayer() {
+        return player;
+    }
+    public void setPlayer(PlayerData player) {
+        this.player = player;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Move other = (Move) obj;
+        if (isFactory == null) {
+            if (other.isFactory != null)
+                return false;
+        } else if (!isFactory.equals(other.isFactory))
+            return false;
+        if (number == null) {
+            if (other.number != null)
+                return false;
+        } else if (!number.equals(other.number))
+            return false;
+        if (type != other.type)
+            return false;
+        if (player == null) {
+            if (other.player != null)
+                return false;
+        } else if (!player.equals(other.player))
+            return false;
+        return true;
+    }
+    
 }
