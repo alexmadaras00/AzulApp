15d1c314de5ec33242907b0953a3e53dfc9a4dde
Serge Johanns <serge.johanns@gmail.com>
2023-05-13 20:48:02 +0200

    Add test stubs for View

---
+++ src/test/java/ViewTest.java
@@ -0,0 +1,63 @@
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import controller.DataObject;
+import controller.IController;
+import view.Messager;
+import view.UI;
+import view.View;
+
+public class ViewTest {
+    private static enum MockDataObject implements DataObject {
+        MOVE,
+        UPDATE;
+
+        @Override
+        public JSONObject toJSON() {
+            return null;
+        }
+
+        @Override
+        public DataObject fromJSON(JSONObject object) {
+            return null;
+        }
+    }
+
+    private static class MockUI implements UI {
+        public boolean updated;
+        public boolean moveRequested;
+
+        @Override
+        public void update(DataObject object) {
+        }
+
+        @Override
+        public DataObject getMove() {
+            return null;
+        }
+    }
+
+    private static class MockController implements IController {
+        public boolean notified;
+
+        @Override
+        public void notify(Object sender, JSONObject message) {
+
+        }
+    }
+
+    private static Messager view;
+    private static MockUI ui;
+    private static IController controller;
+
+    @BeforeAll
+    static void setUp() {
+
+    }
+
+    @Test
+    void testConnectController() {
+
+    }
+}
