36c78d5ac925944e1a6d604696f14ceee8e3e5e5
Serge Johanns <serge.johanns@gmail.com>
2023-05-13 16:43:56 +0200

    Finalize stub implementation for View

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,10 +1,18 @@
 package view;
 
+import controller.DataObject;
 import controller.IController;
 
 public class View implements Messager {
+    private IController controller;
+    private UI userInterface;
+
     @Override
     public void connectController(IController IController) {
+
+    }
+
+    private void sendInput(DataObject object) {
 
     }
 }
