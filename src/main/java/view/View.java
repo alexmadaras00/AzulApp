5a5f1067d3d4c87d18224552bb4ab75559620122
Serge Johanns <serge.johanns@gmail.com>
2023-05-13 22:42:35 +0200

    Implement View

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -8,20 +8,23 @@ public class View implements Messager {
     private UI userInterface;
 
     public View(UI userInterface) {
-
+        this.userInterface = userInterface;
     }
 
     @Override
     public void connectController(IController controller) {
-
+        this.controller = controller;
+        while (true) {
+            sendInput(userInterface.getMove());
+        }
     }
 
     @Override
     public void handleResponse(DataObject message) {
-
+        userInterface.update(message);
     }
 
     private void sendInput(DataObject object) {
-
+        controller.notify(this, object);
     }
 }
