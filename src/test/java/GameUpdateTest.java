c732c9b699324efbdbabe2fbc9745230a28af398
TinyLittleWolf13 <j.c.degroot-3@student.utwente.nl>
2023-05-16 14:32:22 +0200

    P42023T03-78 Redid implementation and tests to conform to new class diagram

--- src/test/java/GameUpdateTest.java
+++ src/test/java/GameUpdateTest.java
@@ -1,49 +1,11 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-
-import com.google.code.beanmatchers.ValueGenerator;
 
 import static com.google.code.beanmatchers.BeanMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import dataobjects.GameUpdate;
 
-import model.Color;
-import model.Tile;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-
 public class GameUpdateTest {
-
-    @BeforeAll
-    public static void setup() {
-        registerValueGenerator(new ValueGenerator<Optional>() {
-            public Optional generate() {
-                if (new Random().nextInt(5) == 1) {
-                    return Optional.ofNullable(null);
-                }
-                if (new Random().nextInt(2) == 1) {
-                    List<List<Tile>> table = new ArrayList<>();
-                    for (int i = 0; i< new Random().nextInt(7); i++) {
-                        table.add(new ArrayList<>());
-                        for (int j = 0; j< new Random().nextInt(7); j++) {
-                            table.get(i).add(Color.values()[new Random().nextInt(Color.values().length)]);
-                        }
-                    }
-                    return Optional.ofNullable(table); 
-                } else {
-                    List<Tile> tiles = new ArrayList<>();
-                    for (int i = 0; i< new Random().nextInt(7); i++) {
-                        tiles.add(Color.values()[new Random().nextInt(Color.values().length)]);
-                    }
-                    return Optional.ofNullable(tiles);
-                }
-            }
-        }, Optional.class);
-    }
 
     @Test
     public void testNoArgsConstructor() {
--- src/test/java/MoveTest.java
+++ src/test/java/MoveTest.java
@@ -1,29 +1,11 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-
-import com.google.code.beanmatchers.ValueGenerator;
 
 import static com.google.code.beanmatchers.BeanMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import dataobjects.Move;
 
-import java.util.OptionalInt;
-import java.util.Random;
-
 public class MoveTest {
-
-    @BeforeAll
-    public static void setup() {
-        registerValueGenerator(new ValueGenerator<OptionalInt>() {
-            public OptionalInt generate() {
-                if (new Random().nextInt(5) == 1) {
-                    return OptionalInt.empty();
-                }
-                return OptionalInt.of(new Random().nextInt(100)); // Change to generate random instance
-            }
-        }, OptionalInt.class);
-    }
 
     @Test
     public void testNoArgsConstructor() {
