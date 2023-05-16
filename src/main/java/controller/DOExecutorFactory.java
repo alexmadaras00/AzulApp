6cab1c6c3290f98da33d9bd52ab065f8e3ec0a59
MrDurion <staneman59@gmail.com>
2023-05-16 22:11:16 +0200

    Added test layout for DOExecutorFactory

---
+++ src/main/java/controller/DOExecutorFactory.java
@@ -0,0 +1,12 @@
+package controller;
+
+import dataobjects.DataObject;
+import dataobjects.Move;
+
+public class DOExecutorFactory implements ExecutorFactory {
+
+    public Executor createExecutor(DataObject message) {
+        return null;
+    }
+
+}
