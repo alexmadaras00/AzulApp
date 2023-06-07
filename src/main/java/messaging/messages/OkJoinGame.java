f92b10156dd87183d8d4af2fe737f9897a54eb31
MrDurion <staneman59@gmail.com>
2023-06-07 17:50:28 +0200

    okJoinGame response

--- src/main/java/messaging/messages/OkJoinGame.java
+++ src/main/java/messaging/messages/OkJoinGame.java
@@ -1,9 +1,20 @@
 package messaging.messages;
 
+import messaging.dataobjects.PlayerData;
+
 public class OkJoinGame extends Response {
+    private PlayerData playerData;
 
     public OkJoinGame(String requestId) {
         super(requestId);
+    }
+
+    public void setPlayerData(PlayerData playerData) {
+        this.playerData = playerData;
+    }
+
+    public PlayerData getPlayerData() {
+        return playerData;
     }
 
 }
