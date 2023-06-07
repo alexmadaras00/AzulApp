94400bceeaf9cb1dd874d3814669c5fab02ed0fb
MrDurion <staneman59@gmail.com>
2023-06-07 20:21:08 +0200

    okStartGame

--- src/test/java/unit/messaging/messages/OkStartGameTest.java
+++ src/test/java/unit/messaging/messages/OkStartGameTest.java
@@ -1,16 +1,37 @@
 package unit.messaging.messages;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import messaging.dataobjects.GameState;
+import messaging.messages.OkStartGame;
+
 public class OkStartGameTest {
+    private OkStartGame response;
+    private String requestId = "1";
+    private static GameState gameState;
+
+    @BeforeAll
+    public static void base() {
+        gameState = new GameState();
+    }
+
     @BeforeEach
     public void setup() {
-
+        response = new OkStartGame(requestId);
     }
 
     @Test
-    public void test1() {
+    public void testRequestId() {
+        assertEquals(requestId, response.getRequestId());
+    }
 
+    @Test
+    public void testSetData() {
+        response.setGameState(gameState);
+        assertEquals(gameState, response.getGameState());
     }
 }
