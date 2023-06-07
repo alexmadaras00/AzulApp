9115a94acf23cc8859ab71206168f505c7400a93
MrDurion <staneman59@gmail.com>
2023-06-07 17:53:16 +0200

    okstartGame response

--- src/main/java/messaging/messages/OkStartGame.java
+++ src/main/java/messaging/messages/OkStartGame.java
@@ -1,9 +1,20 @@
 package messaging.messages;
 
+import messaging.dataobjects.GameState;
+
 public class OkStartGame extends Response {
+    private GameState gameState;
 
     public OkStartGame(String requestId) {
         super(requestId);
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
     }
 
 }
