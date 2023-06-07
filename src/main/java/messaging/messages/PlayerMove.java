dd014b5552068c2255fbcc2cb2a777aab8634d37
MrDurion <staneman59@gmail.com>
2023-06-07 18:01:04 +0200

    playerMove

--- src/main/java/messaging/messages/PlayerMove.java
+++ src/main/java/messaging/messages/PlayerMove.java
@@ -7,15 +7,13 @@ public class PlayerMove extends Request {
     private PlayerData player;
     private Action action;
 
-    public PlayerMove() {
+    public PlayerMove(PlayerData player) {
+        super();
+        this.player = player;
     }
 
     public PlayerData getPlayer() {
         return player;
-    }
-
-    public void setPlayer(PlayerData player) {
-        this.player = player;
     }
 
     public Action getAction() {
@@ -24,19 +22,5 @@ public class PlayerMove extends Request {
 
     public void setAction(Action action) {
         this.action = action;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof PlayerMove))
-            return false;
-
-        PlayerMove that = (PlayerMove) o;
-
-        if (getPlayer() != null ? !getPlayer().equals(that.getPlayer()) : that.getPlayer() != null)
-            return false;
-        return getAction() != null ? getAction().equals(that.getAction()) : that.getAction() == null;
     }
 }
