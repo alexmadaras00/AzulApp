47fc761a855208156bce70cfc44ee1dd0d138b5d
MrDurion <staneman59@gmail.com>
2023-06-07 20:11:12 +0200

    badrequest

--- src/test/java/unit/messaging/messages/BadRequestTest.java
+++ src/test/java/unit/messaging/messages/BadRequestTest.java
@@ -1,17 +1,30 @@
 package unit.messaging.messages;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BadRequestTest {
+import messaging.messages.BadRequest;
 
+public class BadRequestTest {
+    private BadRequest request;
+    private String id = "1";
+    private String reason = "Great Test";
     @BeforeEach
     public void setup() {
-
+        request = new BadRequest(id);
     }
 
     @Test
-    public void test1() {
-        
+    public void testRequestId() {
+        assertEquals(id, request.getRequestId());
     }
+
+    @Test
+    public void testReason() {
+        request.setReason(reason);
+        assertEquals(reason, request.getReason());
+    }
+    
 }
