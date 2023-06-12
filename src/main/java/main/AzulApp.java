03cc3724ebc5fa2e8da2dbacb04f9219df3a0e55
MrDurion <staneman59@gmail.com>
2023-06-12 23:23:49 +0200

    testing in main

--- src/main/java/main/AzulApp.java
+++ src/main/java/main/AzulApp.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import controller.Controller;
 import controller.Mediator;
 import javafx.application.Application;
@@ -8,6 +12,7 @@ import messaging.executors.ConcreteExecutorFactory;
 import messaging.executors.ExecutorFactory;
 import model.Game;
 import model.Model;
+import model.TileColor;
 import view.GUI;
 import view.Messager;
 import view.UI;
@@ -33,6 +38,13 @@ public class AzulApp extends Application {
         messager.connectMediator(mediator);
         messager.connectUI(gui);
         gui.start(stage);
+        gui.factorySetup(List.of(1, 2, 3, 4, 5));
+        gui.addPlayer(15, "Henk de ST");
+        gui.addPlayer(14, "Bob the buoder");
+        gui.setTilesFactory(2, List.of(TileColor.BLUE, TileColor.BLUE, TileColor.RED, TileColor.YELLOW));
+        gui.setTilesFactory(4, List.of(TileColor.BLUE, TileColor.CYAN, TileColor.RED, TileColor.BLACK));
+        gui.startGame();
+        gui.clearFactory(2);
     }
 
 }
