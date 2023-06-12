f9ef87c33e4d0178e70c052c930c0ec695b93bb2
TinyLittleWolf13 <j.c.degroot-3@student.utwente.nl>
2023-06-12 16:03:13 +0200

    Fixed stub implementation of model for tests ControllerTest and DataObjectExecutorFactoryTest

--- src/test/java/integration/controller/ControllerTest.java
+++ src/test/java/integration/controller/ControllerTest.java
@@ -21,170 +21,165 @@ import view.UI;
 
 public class ControllerTest {
 
-    // private enum MockDataObject implements DataObject {
-    //     MOVE, UPDATE;
-    // }
+    private enum MockDataObject implements DataObject {
+        MOVE, UPDATE;
+    }
 
-    // private static class MockMessager implements Messager {
-    //     public List<DataObject> notifications = new LinkedList<DataObject>();
-    //     private Mediator mediator;
+    private static class MockMessager implements Messager {
+        public List<DataObject> notifications = new LinkedList<DataObject>();
+        private Mediator mediator;
 
-    //     @Override
-    //     public void connectMediator(Mediator mediator) {
-    //         this.mediator = mediator;
-    //     }
+        @Override
+        public void connectMediator(Mediator mediator) {
+            this.mediator = mediator;
+        }
 
-    //     @Override
-    //     public void notify(DataObject message) {
-    //         notifications.add(message);
-    //     }
+        @Override
+        public void notify(DataObject message) {
+            notifications.add(message);
+        }
 
-    //     @Override
-    //     public void send(DataObject message) {
-    //         mediator.notify(message);
-    //     }
+        @Override
+        public void send(DataObject message) {
+            mediator.notify(message);
+        }
 
-    //     @Override
-    //     public void connectUI(UI userInterface) {
-    //         return;
-    //     }
-    // }
+        @Override
+        public void connectUI(UI userInterface) {
+            return;
+        }
+    }
 
-    // private static class MockModel implements Model {
-    //     public int didSomethingCounter;
+    private static class MockModel implements Model {
+        public int didSomethingCounter;
 
-    //     public MockModel() {
-    //         didSomethingCounter = 0;
-    //     }
+        public MockModel() {
+            didSomethingCounter = 0;
+        }
 
-    //     public DataObject doSomething() {
-    //         didSomethingCounter++;
-    //         return MockDataObject.UPDATE;
-    //     }
+        public DataObject doSomething() {
+            didSomethingCounter++;
+            return MockDataObject.UPDATE;
+        }
 
-    //     @Override
-    //     public GameState startGame() {
-    //         return null;
-    //     }
+        @Override
+        public GameState startGame() {
+            return null;
+        }
 
-    //     @Override
-    //     public RoundUpdate startRound() {
-    //         return null;
-    //     }
+        @Override
+        public GameState terminateGame() {
+            return null;
+        }
 
-    //     @Override
-    //     public RoundUpdate endRound() {
-    //         return null;
-    //     }
+        @Override
+        public boolean isCurrentPlayer(PlayerData player) {
+            return false;
+        }
 
-    //     @Override
-    //     public GameState terminateGame() {
-    //         return null;
-    //     }
+        @Override
+        public PlayerData addPlayer(Player playerData) {
+            return null;
+        }
 
-    //     @Override
-    //     public GameState endGame() {
-    //         return null;
-    //     }
+        @Override
+        public boolean isValidStartGame() {
+            return false;
+        }
 
-    //     @Override
-    //     public boolean isCurrentPlayer(PlayerData player) {
-    //         return false;
-    //     }
+        @Override
+        public boolean isValidMoveFactoryFloorLine(int factoryIndex, TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return false;
+        }
 
-    //     @Override
-    //     public PlayerData addPlayer(Player playerData) {
-    //         return null;
-    //     }
+        @Override
+        public boolean isValidMoveFactoryPatternLine(int factoryIndex, int patternLineRow, TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return false;
+        }
 
-    //     @Override
-    //     public boolean isValidStartGame() {
-    //         return false;
-    //     }
+        @Override
+        public boolean isValidMoveMiddleFloorLine(TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return false;
+        }
 
-    //     @Override
-    //     public DataObject performMoveFactoryPatternLine(List<Tile> tiles, int factoryIndex, int patternLineRow, TileColor tileColor) {
-    //         return null;
-    //     }
+        @Override
+        public boolean isValidMoveMiddlePatternLine(int patternLineRow, TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return false;
+        }
 
-    //     @Override
-    //     public MoveUpdate performMoveMiddlePatternLine(List<Tile> tiles, int patternLineRow, TileColor tileColor) {
-    //         return null;
-    //     }
+        @Override
+        public DataObject performMoveFactoryFloorLine(int factoryIndex, TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-    //     @Override
-    //     public MoveUpdate performMoveFactoryFloorLine(List<Tile> tiles, int factoryIndex) {
-    //         return null;
-    //     }
+        @Override
+        public DataObject performMoveFactoryPatternLine(int factoryIndex, int patternLineRow, TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-    //     @Override
-    //     public MoveUpdate performMoveMiddleFloorLine(List<Tile> tiles) {
-    //         return null;
-    //     }
+        @Override
+        public DataObject performMoveMiddleFloorLine(TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-    //     @Override
-    //     public boolean isValidMoveFactoryPatternLine(List<Tile> tiles, int factoryIndex, int patternLineRow, TileColor tileColor) {
-    //         return false;
-    //     }
+        @Override
+        public DataObject performMoveMiddlePatternLine(int patternLineRow, TileColor tileColor) {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-    //     @Override
-    //     public boolean isValidMoveFactoryFloorLine(List<Tile> tiles, int factoryIndex) {
-    //         return false;
-    //     }
+        
 
-    //     @Override
-    //     public boolean isValidMoveMiddlePatternLine(List<Tile> tiles, int patternLineRow, TileColor tileColor) {
-    //         return false;
-    //     }
+    }
 
-    //     @Override
-    //     public boolean isValidMoveMiddleFloorLine(List<Tile> tiles) {
-    //         return false;
-    //     }
+    private static class MockExecutor implements Executor {
 
-    // }
+        public MockExecutor(DataObject message) {}
 
-    // private static class MockExecutor implements Executor {
+        @Override
+        public DataObject execute(Executable model) {
+            MockModel newModel = (ControllerTest.MockModel) model;
+            return newModel.doSomething();
+        }
 
-    //     public MockExecutor(DataObject message) {}
+    }
 
-    //     @Override
-    //     public DataObject execute(Executable model) {
-    //         MockModel newModel = (ControllerTest.MockModel) model;
-    //         return newModel.doSomething();
-    //     }
+    private static class MockExecutorFactory implements ExecutorFactory {
 
-    // }
+        @Override
+        public Executor createExecutor(DataObject message) {
+            return new MockExecutor(message);
+        }
+    }
 
-    // private static class MockExecutorFactory implements ExecutorFactory {
+    private static Mediator controller;
+    private static MockModel model;
+    private static MockMessager messager;
 
-    //     @Override
-    //     public Executor createExecutor(DataObject message) {
-    //         return new MockExecutor(message);
-    //     }
-    // }
+    @BeforeAll
+    public static void setUp() {
+        model = new MockModel();
+        messager = new MockMessager();
+        controller = new Controller(new MockExecutorFactory());
+        messager.connectMediator(controller);
+        controller.connectMessager(messager);
+        controller.connectModel(model);
+    }
 
-    // private static Mediator controller;
-    // private static MockModel model;
-    // private static MockMessager messager;
-
-    // @BeforeAll
-    // public static void setUp() {
-    //     model = new MockModel();
-    //     messager = new MockMessager();
-    //     controller = new Controller(new MockExecutorFactory());
-    //     messager.connectMediator(controller);
-    //     controller.connectMessager(messager);
-    //     controller.connectModel(model);
-    // }
-
-    // @Test
-    // public void testConnection() {
-    //     assertEquals(0, model.didSomethingCounter);
-    //     assertEquals(0, messager.notifications.size());
-    //     messager.send(MockDataObject.MOVE);
-    //     assertEquals(1, model.didSomethingCounter);
-    //     assertEquals(1, messager.notifications.size());
-    //     assertEquals(MockDataObject.UPDATE, messager.notifications.get(0));
-    // }
+    @Test
+    public void testConnection() {
+        assertEquals(0, model.didSomethingCounter);
+        assertEquals(0, messager.notifications.size());
+        messager.send(MockDataObject.MOVE);
+        assertEquals(1, model.didSomethingCounter);
+        assertEquals(1, messager.notifications.size());
+        assertEquals(MockDataObject.UPDATE, messager.notifications.get(0));
+    }
 }
