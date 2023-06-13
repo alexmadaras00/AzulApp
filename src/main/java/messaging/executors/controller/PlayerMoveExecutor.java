2500db23ea27fb0da8d579ed09f90f66551faa97
MrDurion <staneman59@gmail.com>
2023-06-13 09:56:39 +0200

    merged new executors

--- src/main/java/messaging/executors/controller/PlayerMoveExecutor.java
+++ src/main/java/messaging/executors/controller/PlayerMoveExecutor.java
@@ -1,17 +1,140 @@
 package messaging.executors.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import messaging.dataobjects.Action;
+import messaging.dataobjects.DataObject;
+import messaging.dataobjects.Location;
+import messaging.dataobjects.LocationType;
+import messaging.dataobjects.MoveUpdate;
+import messaging.dataobjects.PlayerData;
+import messaging.dataobjects.RoundUpdate;
 import messaging.executors.Executable;
 import messaging.executors.Executor;
 import messaging.messages.Message;
+import messaging.messages.NotOkJoinGame;
+import messaging.messages.NotOkPlayerMove;
+import messaging.messages.OkPlayerMove;
 import messaging.messages.PlayerMove;
+import model.Model;
+import model.Player;
+import model.Tile;
 
 public class PlayerMoveExecutor implements Executor {
     private PlayerMove message;
 
+    public static final String reasonNotAPlayer = "The player provided does not exist.";
+    public static final String reasonNotAnAction = "The action provided is not valid.";
+    public static final String reasonNotTheirTurn = "Not their turn.";
+    public static final String reasonActionNotAllowed = "The action provided is not allowed.";
+
+    private Location from;
+    private Location to;
+    private Model model;
+
+    private boolean isInGame(PlayerData playerData) {
+        for (Player player : model.getPlayers()) {
+            if (player.getIdentifier() == playerData.getIdentifier() && player.getName() == playerData.getName()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidAction(Action action) {
+        if (action.getColor() == null || action.getFrom() == null || action.getTo() == null) {
+            return false;
+        }
+        from = action.getFrom();
+        to = action.getTo();
+
+        List<LocationType> validFromLocations = Arrays.asList(LocationType.FACTORY, LocationType.MIDDLE);
+        List<LocationType> validToLocations = Arrays.asList(LocationType.PATTERN_LINE,
+                LocationType.FLOOR_LINE);
+        if (!validFromLocations.contains(from.getType()) || !validToLocations.contains(to.getType())) {
+            return false;
+        }
+        if ()
+        return true;
+    }
+
+    private boolean canPlayAction(Action action) {
+        switch (from.getType()) {
+            case FACTORY:
+                List<Tile> tiles = model.getFactories().get(from.getIndex());
+                switch (to.getType()) {
+                    case FACTORY:
+
+                        model.isValidMoveFactoryFloorLine(tiles, 0);
+                        break;
+                    case MIDDLE:
+                        break;
+                    default:
+                        return false;
+                }
+                break;
+            case MIDDLE:
+                switch (to.getType()) {
+                    case PATTERN_LINE:
+                        break;
+                    case FLOOR_LINE:
+                        break;
+                    default:
+                        return false;
+                }
+                break;
+            default:
+                return false;
+        }
+        return false;
+    }
+
+    private DataObject performMove(PlayerData playerData, Action action) {
+        return null;
+    }
+
     @Override
     public Message execute(Executable executable) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        if (!(executable instanceof Model) || message == null) {
+            return null;
+        }
+        model = (Model) executable;
+
+        PlayerData playerData = message.getPlayer();
+        if (!isInGame(playerData)) {
+            NotOkPlayerMove response = new NotOkPlayerMove(message.getId());
+            response.setReason(reasonNotAPlayer);
+            return response;
+        }
+
+        if (!model.isCurrentPlayer(playerData)) {
+            NotOkPlayerMove response = new NotOkPlayerMove(message.getId());
+            response.setReason(reasonNotTheirTurn);
+            return response;
+        }
+        Action action = message.getAction();
+        if (!isValidAction(action)) {
+            NotOkPlayerMove response = new NotOkPlayerMove(message.getId());
+            response.setReason(reasonNotAnAction);
+            return response;
+        }
+
+        if (!canPlayAction(action)) {
+            NotOkPlayerMove response = new NotOkPlayerMove(message.getId());
+            response.setReason(reasonActionNotAllowed);
+            return response;
+        }
+        OkPlayerMove response = new OkPlayerMove(message.getId());
+        DataObject dataObject = performMove(playerData, action);
+        response.setDataObject(dataObject);
+        if (dataObject instanceof MoveUpdate) {
+            response.setEndOfRound(false);
+        } else if (dataObject instanceof RoundUpdate) {
+            response.setEndOfRound(true);
+
+        }
+        return response;
     }
 
     @Override
--- src/test/java/integration/messaging/executors/PlayerMoveExecutorTest.java
+++ src/test/java/integration/messaging/executors/PlayerMoveExecutorTest.java
@@ -1,27 +1,239 @@
 package integration.messaging.executors;
 
-import model.Game;
-import model.Model;
-import model.Player;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import messaging.dataobjects.Action;
+import messaging.dataobjects.Location;
+import messaging.dataobjects.LocationType;
+import messaging.dataobjects.PlayerData;
 import messaging.executors.controller.PlayerMoveExecutor;
+import messaging.messages.Message;
+import messaging.messages.NotOkPlayerMove;
+import messaging.messages.OkPlayerMove;
+import messaging.messages.PlayerMove;
+import model.Game;
+import model.Model;
+import model.Player;
+import model.TileColor;
 
 public class PlayerMoveExecutorTest {
-    Model model;
-    static Player player1;
-    static Player player2;
-    static PlayerMoveExecutor playerMoveExecutor;
+    private Model model;
+    private PlayerMoveExecutor executor;
+    private Message message;
+    private Action action;
+    private Player player1;
+    private Player player2;
+    private PlayerData playerData;
+    private Location from;
+    private Location to;
+    private TileColor tileColor;
 
     @BeforeEach
-    public void setUp() {
+    public void setup() {
+        model = new Game();
+        executor = new PlayerMoveExecutor();
+        player1 = new Player("Mika");
+        player2 = new Player("Ghost");
+        model.addPlayer(player1);
+        model.addPlayer(player2);
+        model.startGame();
+        model.startRound();
+        if (!(model.getTurnOrder().get(0) == player1)) {
+            player1 = player2;
+            player2 = model.getTurnOrder().get(0);
+        }
+        playerData = new PlayerData();
+        playerData.setIdentifier(player1.getIdentifier());
+        playerData.setName(player1.getName());
+        message = new PlayerMove(playerData);
+        action = new Action();
+        tileColor = model.getFactories().get(1).getAllTiles().get(0);
+        from = new Location();
+        from.setType(LocationType.FACTORY);
+        from.setIndex(1);
+        to = new Location();
+        to.setType(LocationType.PATTERN_LINE);
+        to.setIndex(3);
+        action.setColor(tileColor);
+        action.setFrom(from);
+        action.setTo(to);
+        ((PlayerMove) message).setAction(action);
+    }
 
+    // Executor tests
+    @Test
+    public void testSetMesage() {
+        assertDoesNotThrow(() -> {
+            executor.setMessage(message);
+        });
     }
 
     @Test
-    public void testExecute() {
+    public void testNoMessageNoExecute() {
+        Message response = executor.execute(model);
+        assertEquals(null, response);
+    }
 
+    @Test
+    public void testWrongMessageNoExecute() {
+        executor.setMessage(new OkPlayerMove("1"));
+        Message response = executor.execute(model);
+        assertEquals(null, response);
+    }
+
+    @Test
+    public void testnoModelNoExecute() {
+        executor.setMessage(message);
+        Message response = executor.execute(null);
+        assertEquals(null, response);
+    }
+
+    // PlayerData Tests
+    @Test
+    public void testNotAPlayerPlayerNullNotOkPlayerMove() {
+        message = new PlayerMove(null);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAPlayer, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAPlayerNameNullNotOkPlayerMove() {
+        playerData.setName(null);
+        message = new PlayerMove(playerData);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAPlayer, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAPlayerIdNullNotOkPlayerMove() {
+        playerData.setName(null);
+        message = new PlayerMove(playerData);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAPlayer, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAPlayerWrongNameNotOkPlayerMove() {
+        playerData.setName("NotInGame");
+        message = new PlayerMove(playerData);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAPlayer, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAPlayerWrongIdNotOkPlayerMove() {
+        playerData.setIdentifier(-1);
+        message = new PlayerMove(playerData);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAPlayer, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotTheirTurnNotOkPlayerMove() {
+        playerData.setName(player2.getName());
+        playerData.setIdentifier(player2.getIdentifier());
+        message = new PlayerMove(playerData);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotTheirTurn, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    // Action tests
+
+    @Test
+    public void testNotAnActionNullNotOkPlayerMove() {
+        ((PlayerMove) message).setAction(null);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAnAction, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAnActionColorNullNotOkPlayerMove() {
+        action.setColor(null);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAnAction, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAnActionFromNullNotOkPlayerMove() {
+        action.setFrom(null);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAnAction, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testNotAnActionToNullNotOkPlayerMove() {
+        action.setTo(null);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAnAction, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testActionNotAllowedNotOkPlayerMove() {
+        action.setFrom(to);
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonActionNotAllowed, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
+    }
+
+    @Test
+    public void testActionNotPossibleNotOkPlayerMove() {
+        for (TileColor tc : TileColor.values()) {
+            if (!model.getFactories().get(1).getAllTiles().contains(tc)) {
+                action.setColor(tc);
+                break;
+            }
+        }
+        ((PlayerMove) message).setAction(action);
+        executor.setMessage(message);
+        Message response = executor.execute(model);
+        assertEquals(true, response instanceof NotOkPlayerMove);
+        assertEquals(PlayerMoveExecutor.reasonNotAnAction, ((NotOkPlayerMove) response).getReason());
+        assertEquals(message.getId(), ((NotOkPlayerMove) response).getRequestId());
     }
 
 }
