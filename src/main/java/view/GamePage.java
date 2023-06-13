e456909e1eefcac18bce246a0c2a7de7f3d88931
MrDurion <staneman59@gmail.com>
2023-06-13 16:18:55 +0200

    added setup of factories

--- src/main/java/view/GamePage.java
+++ src/main/java/view/GamePage.java
@@ -39,50 +39,6 @@ public class GamePage {
         this.controller = controller;
     }
 
-
-    public static Color translateColor(Tile tile) {
-        if (!(tile instanceof TileColor)) {
-            return null;
-        }
-        switch ((TileColor) tile) {
-            case RED:
-                return Color.RED;
-            case BLACK:
-                return Color.BLACK;
-
-            case BLUE:
-                return Color.BLUE;
-
-            case YELLOW:
-                return Color.YELLOW;
-
-            case CYAN:
-                return Color.CYAN;
-
-            default:
-                return null;
-        }
-    }
-
-    public static TileColor translateColorBack(Color color) {
-        if (color == Color.RED) {
-            return TileColor.RED;
-        } else if (color == Color.BLACK) {
-            return TileColor.BLACK;
-
-        } else if (color == Color.BLUE) {
-            return TileColor.BLUE;
-
-        } else if (color == Color.YELLOW) {
-            return TileColor.YELLOW;
-
-        } else if (color == Color.CYAN) {
-            return TileColor.CYAN;
-        }
-        return null;
-
-    }
-
     @FXML
     private Button buttonF1T1;
     @FXML
@@ -289,208 +245,207 @@ public class GamePage {
     private VBox playerboard4;
 
     // class PlayerGUI {
-    //     private int place;
-    //     private int id;
+    // private int place;
+    // private int id;
 
-    //     public PlayerGUI(int place, int id) {
-    //         this.place = place;
-    //         this.id = id;
-    //     }
-
-    //     int getId() {
-    //         return this.id;
-    //     }
-
-    //     GridPane getWall(int row) {
-    //         switch (place) {
-    //             case 1:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player1W1;
-    //                     case 1:
-    //                         return player1W2;
-    //                     case 2:
-    //                         return player1W3;
-    //                     case 3:
-    //                         return player1W4;
-    //                     case 4:
-    //                         return player1W5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             case 2:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player2W1;
-    //                     case 1:
-    //                         return player2W2;
-    //                     case 2:
-    //                         return player2W3;
-    //                     case 3:
-    //                         return player2W4;
-    //                     case 4:
-    //                         return player2W5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             case 3:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player3W1;
-    //                     case 1:
-    //                         return player3W2;
-    //                     case 2:
-    //                         return player3W3;
-    //                     case 3:
-    //                         return player3W4;
-    //                     case 4:
-    //                         return player3W5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             case 4:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player4W1;
-    //                     case 1:
-    //                         return player4W2;
-    //                     case 2:
-    //                         return player4W3;
-    //                     case 3:
-    //                         return player4W4;
-    //                     case 4:
-    //                         return player4W5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             default:
-    //                 return null;
-    //         }
-    //     }
-
-    //     GridPane getPatternline(int row) {
-    //         switch (place) {
-    //             case 1:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player1PL1;
-    //                     case 1:
-    //                         return player1PL2;
-    //                     case 2:
-    //                         return player1PL3;
-    //                     case 3:
-    //                         return player1PL4;
-    //                     case 4:
-    //                         return player1PL5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             case 2:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player2PL1;
-    //                     case 1:
-    //                         return player2PL2;
-    //                     case 2:
-    //                         return player2PL3;
-    //                     case 3:
-    //                         return player2PL4;
-    //                     case 4:
-    //                         return player2PL5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             case 3:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player3PL1;
-    //                     case 1:
-    //                         return player3PL2;
-    //                     case 2:
-    //                         return player3PL3;
-    //                     case 3:
-    //                         return player3PL4;
-    //                     case 4:
-    //                         return player3PL5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             case 4:
-    //                 switch (row) {
-    //                     case 0:
-    //                         return player4PL1;
-    //                     case 1:
-    //                         return player4PL2;
-    //                     case 2:
-    //                         return player4PL3;
-    //                     case 3:
-    //                         return player4PL4;
-    //                     case 4:
-    //                         return player4PL5;
-    //                     default:
-    //                         return null;
-    //                 }
-    //             default:
-    //                 return null;
-    //         }
-    //     }
-
-    //     HBox getFloorline() {
-    //         switch (place) {
-    //             case 1:
-    //                 return player1Floor;
-    //             case 2:
-    //                 return player2Floor;
-
-    //             case 3:
-    //                 return player3Floor;
-
-    //             case 4:
-    //                 return player4Floor;
-
-    //             default:
-    //                 return null;
-    //         }
-    //     }
-
-    //     Label getScore() {
-    //         switch (place) {
-    //             case 1:
-    //                 return player1Score;
-    //             case 2:
-    //                 return player2Score;
-
-    //             case 3:
-    //                 return player3Score;
-
-    //             case 4:
-    //                 return player4Score;
-
-    //             default:
-    //                 return null;
-    //         }
-    //     }
-
-    //     Label getName() {
-    //         switch (place) {
-    //             case 1:
-    //                 return player1Name;
-    //             case 2:
-    //                 return player2Name;
-
-    //             case 3:
-    //                 return player3Name;
-
-    //             case 4:
-    //                 return player4Name;
-
-    //             default:
-    //                 return null;
-    //         }
-    //     }
-
+    // public PlayerGUI(int place, int id) {
+    // this.place = place;
+    // this.id = id;
     // }
 
+    // int getId() {
+    // return this.id;
+    // }
+
+    // GridPane getWall(int row) {
+    // switch (place) {
+    // case 1:
+    // switch (row) {
+    // case 0:
+    // return player1W1;
+    // case 1:
+    // return player1W2;
+    // case 2:
+    // return player1W3;
+    // case 3:
+    // return player1W4;
+    // case 4:
+    // return player1W5;
+    // default:
+    // return null;
+    // }
+    // case 2:
+    // switch (row) {
+    // case 0:
+    // return player2W1;
+    // case 1:
+    // return player2W2;
+    // case 2:
+    // return player2W3;
+    // case 3:
+    // return player2W4;
+    // case 4:
+    // return player2W5;
+    // default:
+    // return null;
+    // }
+    // case 3:
+    // switch (row) {
+    // case 0:
+    // return player3W1;
+    // case 1:
+    // return player3W2;
+    // case 2:
+    // return player3W3;
+    // case 3:
+    // return player3W4;
+    // case 4:
+    // return player3W5;
+    // default:
+    // return null;
+    // }
+    // case 4:
+    // switch (row) {
+    // case 0:
+    // return player4W1;
+    // case 1:
+    // return player4W2;
+    // case 2:
+    // return player4W3;
+    // case 3:
+    // return player4W4;
+    // case 4:
+    // return player4W5;
+    // default:
+    // return null;
+    // }
+    // default:
+    // return null;
+    // }
+    // }
+
+    // GridPane getPatternline(int row) {
+    // switch (place) {
+    // case 1:
+    // switch (row) {
+    // case 0:
+    // return player1PL1;
+    // case 1:
+    // return player1PL2;
+    // case 2:
+    // return player1PL3;
+    // case 3:
+    // return player1PL4;
+    // case 4:
+    // return player1PL5;
+    // default:
+    // return null;
+    // }
+    // case 2:
+    // switch (row) {
+    // case 0:
+    // return player2PL1;
+    // case 1:
+    // return player2PL2;
+    // case 2:
+    // return player2PL3;
+    // case 3:
+    // return player2PL4;
+    // case 4:
+    // return player2PL5;
+    // default:
+    // return null;
+    // }
+    // case 3:
+    // switch (row) {
+    // case 0:
+    // return player3PL1;
+    // case 1:
+    // return player3PL2;
+    // case 2:
+    // return player3PL3;
+    // case 3:
+    // return player3PL4;
+    // case 4:
+    // return player3PL5;
+    // default:
+    // return null;
+    // }
+    // case 4:
+    // switch (row) {
+    // case 0:
+    // return player4PL1;
+    // case 1:
+    // return player4PL2;
+    // case 2:
+    // return player4PL3;
+    // case 3:
+    // return player4PL4;
+    // case 4:
+    // return player4PL5;
+    // default:
+    // return null;
+    // }
+    // default:
+    // return null;
+    // }
+    // }
+
+    // HBox getFloorline() {
+    // switch (place) {
+    // case 1:
+    // return player1Floor;
+    // case 2:
+    // return player2Floor;
+
+    // case 3:
+    // return player3Floor;
+
+    // case 4:
+    // return player4Floor;
+
+    // default:
+    // return null;
+    // }
+    // }
+
+    // Label getScore() {
+    // switch (place) {
+    // case 1:
+    // return player1Score;
+    // case 2:
+    // return player2Score;
+
+    // case 3:
+    // return player3Score;
+
+    // case 4:
+    // return player4Score;
+
+    // default:
+    // return null;
+    // }
+    // }
+
+    // Label getName() {
+    // switch (place) {
+    // case 1:
+    // return player1Name;
+    // case 2:
+    // return player2Name;
+
+    // case 3:
+    // return player3Name;
+
+    // case 4:
+    // return player4Name;
+
+    // default:
+    // return null;
+    // }
+    // }
+
+    // }
 
     @FXML
     void selectTile(ActionEvent event) {
@@ -508,29 +463,139 @@ public class GamePage {
         // TODO - send Message
     }
 
+    public static Color translateColor(Tile tile) {
 
+    }
+
+    public static TileColor translateColorBack(Color color) {
+        if (color == Color.RED) {
+            return TileColor.RED;
+        } else if (color == Color.BLACK) {
+            return TileColor.BLACK;
+
+        } else if (color == Color.BLUE) {
+            return TileColor.BLUE;
+
+        } else if (color == Color.YELLOW) {
+            return TileColor.YELLOW;
+
+        } else if (color == Color.CYAN) {
+            return TileColor.CYAN;
+        }
+        return null;
+
+    }
 
     public void toast(String msg) {
         System.out.println(msg);
     }
 
-    public void update() {
+    private void setTileColor(Button tileButton, Tile tile) {
+        tileButton.setBackground(Background.fill(getColor(tile)));
+    }
+
+    private Paint getColor(Tile tile) {
+        if (tile == null) {
+            return Color.rgb(0, 0, 0, 0);
+        }
+        if (!(tile instanceof TileColor)) {
+            return Color.ANTIQUEWHITE;
+        }
+        switch ((TileColor) tile) {
+            case RED:
+                return Color.RED;
+            case BLACK:
+                return Color.BLACK;
+
+            case BLUE:
+                return Color.BLUE;
+
+            case YELLOW:
+                return Color.YELLOW;
+
+            case CYAN:
+                return Color.CYAN;
+
+            default:
+                return null;
+        }
+    }
+
+    private void setupFactories() {
         List<Factory> factories = model.getFactories();
-        switch (factories.size()) {
-            case 5:
+        if (factories.size() <= 7) {
+            if (factories.size() <= 5) {
                 factory7.setVisible(false);
                 factory6.setVisible(false);
-                // TODO set factory buttons to correct color 
-                Factory factory1model = factories.get(0);
-                buttonF1T1.setBackground(null);
+            }
+            factory9.setVisible(false);
+            factory8.setVisible(false);
+        }
+        List<TileColor> factoryTiles = null;
+
+        switch (factories.size()) {
+            case 9:
+                factoryTiles = factories.get(8).getAllTiles();
+                setTileColor(buttonF9T1, factoryTiles.get(0));
+                setTileColor(buttonF9T2, factoryTiles.get(1));
+                setTileColor(buttonF9T3, factoryTiles.get(2));
+                setTileColor(buttonF9T4, factoryTiles.get(3));
+                factoryTiles = factories.get(7).getAllTiles();
+                setTileColor(buttonF8T1, factoryTiles.get(0));
+                setTileColor(buttonF8T2, factoryTiles.get(1));
+                setTileColor(buttonF8T3, factoryTiles.get(2));
+                setTileColor(buttonF8T4, factoryTiles.get(3));
+
             case 7:
-                factory9.setVisible(false);
-                factory8.setVisible(false);
+                factoryTiles = factories.get(6).getAllTiles();
+                setTileColor(buttonF7T1, factoryTiles.get(0));
+                setTileColor(buttonF7T2, factoryTiles.get(1));
+                setTileColor(buttonF7T3, factoryTiles.get(2));
+                setTileColor(buttonF7T4, factoryTiles.get(3));
+
+                factoryTiles = factories.get(5).getAllTiles();
+                setTileColor(buttonF6T1, factoryTiles.get(0));
+                setTileColor(buttonF6T2, factoryTiles.get(1));
+                setTileColor(buttonF6T3, factoryTiles.get(2));
+                setTileColor(buttonF6T4, factoryTiles.get(3));
+
+            case 5:
+                factoryTiles = factories.get(4).getAllTiles();
+                setTileColor(buttonF5T1, factoryTiles.get(0));
+                setTileColor(buttonF5T2, factoryTiles.get(1));
+                setTileColor(buttonF5T3, factoryTiles.get(2));
+                setTileColor(buttonF5T4, factoryTiles.get(3));
+
+                factoryTiles = factories.get(3).getAllTiles();
+                setTileColor(buttonF4T1, factoryTiles.get(0));
+                setTileColor(buttonF4T2, factoryTiles.get(1));
+                setTileColor(buttonF4T3, factoryTiles.get(2));
+                setTileColor(buttonF4T4, factoryTiles.get(3));
+
+                factoryTiles = factories.get(2).getAllTiles();
+                setTileColor(buttonF3T1, factoryTiles.get(0));
+                setTileColor(buttonF3T2, factoryTiles.get(1));
+                setTileColor(buttonF3T3, factoryTiles.get(2));
+                setTileColor(buttonF3T4, factoryTiles.get(3));
+
+                factoryTiles = factories.get(1).getAllTiles();
+                setTileColor(buttonF2T1, factoryTiles.get(0));
+                setTileColor(buttonF2T2, factoryTiles.get(1));
+                setTileColor(buttonF2T3, factoryTiles.get(2));
+                setTileColor(buttonF2T4, factoryTiles.get(3));
+                factoryTiles = factories.get(0).getAllTiles();
+
+                setTileColor(buttonF1T1, factoryTiles.get(0));
+                setTileColor(buttonF1T2, factoryTiles.get(1));
+                setTileColor(buttonF1T3, factoryTiles.get(2));
+                setTileColor(buttonF1T4, factoryTiles.get(3));
+
             default:
                 return;
         }
     }
 
+    public void update() {
+        setupFactories();
     }
-
 }
