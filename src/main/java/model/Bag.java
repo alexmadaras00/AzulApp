611a96319fea6517d61fadac8b50827646bb6dff
MrDurion <staneman59@gmail.com>
2023-06-13 17:04:28 +0200

    added shuffle to getTiles and popTiles

--- src/main/java/model/Bag.java
+++ src/main/java/model/Bag.java
@@ -1,19 +1,22 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Bag {
     private final List<TileColor> tiles = new ArrayList<>();
 
     public List<TileColor> getTiles() {
+        Collections.shuffle(tiles);
         return tiles;
     }
 
     public List<TileColor> popTiles(int count) {
+        Collections.shuffle(tiles);
         List<TileColor> poppedTiles = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            poppedTiles.add(tiles.get(tiles.size()-1));
+            poppedTiles.add(tiles.get(tiles.size() - 1));
             tiles.remove(tiles.size() - 1);
         }
         return poppedTiles;
