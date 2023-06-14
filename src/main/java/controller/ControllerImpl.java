40003bf98ab833330281d4986013bd1619223a0b
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2023-06-14 16:19:12 +0200

    P42023T03-135: Added the contrller - view direction for the ControllerImpl

--- src/main/java/controller/ControllerImpl.java
+++ src/main/java/controller/ControllerImpl.java
@@ -3,9 +3,10 @@ package controller;
 import model.Model;
 import model.Player;
 import model.TileColor;
+import utils.ExceptionGameStart;
 import view.View;
 
-public class ControllerImpl implements Controller{
+public class ControllerImpl implements Controller {
     private Model model;
     private View view;
 
@@ -13,12 +14,9 @@ public class ControllerImpl implements Controller{
     public void joinPlayer(String name) {
         if (model.isPlaying()) {
             view.toast("Game already playing. Wait until the game is finished.");
-            return;
-        } else if (model.getPlayerList().size() > 4) {
+        } else if (model.getPlayerList().size() >= 4) {
             view.toast("Too many players");
-            return;
-        }
-        else {
+        } else {
             model.addPlayer(new Player(name));
             view.toast(name + " added");
             view.update();
@@ -27,42 +25,67 @@ public class ControllerImpl implements Controller{
 
     @Override
     public void startGame() {
-        model.startGame();
+        if (model.canStartGame()) {
+            model.startGame();
+            view.update();
+            view.toast("Game started");
+        } else {
+            try {
+                model.startGame();
+            } catch (ExceptionGameStart e) {
+                view.toast(e.getMessage());
+            }
+        }
     }
 
     @Override
     public void performMoveMiddleFloorLine(TileColor tileColor) {
-        if(model.isValidMoveMiddleFloorLine(tileColor))
+        if (model.isValidMoveMiddleFloorLine(tileColor)) {
             model.performMoveMiddleFloorLine(tileColor);
-        else throw new RuntimeException("Invalid move.");
+            view.update();
+            view.toast("Performing move... (tile: " + tileColor + " from the Middle to the Floor Line)");
+        } else {
+            throw new RuntimeException("Invalid move.");
+        }
     }
 
     @Override
     public void performMoveMiddlePatternLine(int row, TileColor tileColor) {
-        if(model.isValidMoveMiddlePatternLine(row,tileColor))
-            model.performMoveMiddlePatternLine(row,tileColor);
-        else throw new RuntimeException("Invalid move.");
+        if (model.isValidMoveMiddlePatternLine(row, tileColor)) {
+            model.performMoveMiddlePatternLine(row, tileColor);
+            view.update();
+            view.toast("Performing move... (tile: " + tileColor + " from the Middle to the row " + row + " in the" +
+                    " Pattern Line)");
+        } else throw new RuntimeException("Invalid move.");
     }
 
     @Override
     public void performMoveFactoryFloorLine(int index, TileColor tileColor) {
-        if(model.isValidMoveFactoryFloorLine(index,tileColor))
-            model.performMoveFactoryFloorLine(index,tileColor);
-        else throw new RuntimeException("Invalid move.");
+        if (model.isValidMoveFactoryFloorLine(index, tileColor)) {
+            model.performMoveFactoryFloorLine(index, tileColor);
+            view.update();
+            view.toast("Performing move... (tile: " + tileColor + " from the Factory " + index + " to the row  the " +
+                    "Floor Line)");
+        } else throw new RuntimeException("Invalid move.");
     }
 
     @Override
     public void performMoveFactoryPatternLine(int index, int row, TileColor tileColor) {
-        if(model.isValidMoveFactoryPatternLine(index,row,tileColor))
-            model.performMoveFactoryPatternLine(index,row,tileColor);
-        else throw new RuntimeException("Invalid move.");
+        if (model.isValidMoveFactoryPatternLine(index, row, tileColor)) {
+            model.performMoveFactoryPatternLine(index, row, tileColor);
+            view.update();
+            view.toast("Performing move... (tile: " + tileColor + " from the Factory " + index + " to the row " + row + " in the" +
+                    " Pattern Line)");
+        } else throw new RuntimeException("Invalid move.");
     }
 
     @Override
     public void terminateGame() {
-        if(model.isPlaying())
+        if (model.isPlaying()) {
             model.terminateGame();
-        else throw new RuntimeException("Invalid request.");
+            view.update();
+            view.toast("Terminating game...");
+        } else throw new RuntimeException("Invalid request.");
     }
 
     public void setModel(Model model) {
@@ -72,4 +95,5 @@ public class ControllerImpl implements Controller{
     public void setView(View view) {
         this.view = view;
     }
+
 }
