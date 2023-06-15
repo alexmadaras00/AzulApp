784a37f8b48b092d7f2c571ca298a0dbe0a156ec
MrDurion <staneman59@gmail.com>
2023-06-15 13:39:02 +0200

    remove unused imports

--- src/main/java/view/GamePage.java
+++ src/main/java/view/GamePage.java
@@ -16,7 +16,6 @@ import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
